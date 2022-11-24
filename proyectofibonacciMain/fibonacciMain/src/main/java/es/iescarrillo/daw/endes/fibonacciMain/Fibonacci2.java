fbd544cc0abe091c2a545c37db019e2df7c30aaa
claralcala <116264758+claralcala@users.noreply.github.com>
2022-11-24 18:05:04 +0100

    Add files via upload

---
+++ proyectofibonacciMain/fibonacciMain/src/main/java/es/iescarrillo/daw/endes/fibonacciMain/Fibonacci2.java
@@ -0,0 +1,12 @@
+package es.iescarrillo.daw.endes.fibonacciMain;
+
+import es.iescarrillo.daw.endes.fibonacci.*;
+
+public class Fibonacci2 {
+
+	public static void main(String[] args) {
+		Fibonacci.main(null);
+
+	}
+
+}
